0974cc9cbac877ce3885681e392061612bd8294f
Jahaziel Israel <blaquebeardcoder@gmail.com>
2020-11-25 18:16:21 -0500

    First few questions answered

--- main.js
+++ main.js
@@ -18,9 +18,13 @@ const carBrands = [
  * e.g., reverseString('cat') => 'tac'
  */
 
+
+
 // 3.
 // Write a function that takes an array of strings,
 // and returns the longest string in the array
+
+
 
 // 4.
 // Using Reduce,
@@ -45,10 +49,47 @@ console.log(shop(wishlist));
 //Make sure to detail the steps that make up a recursive algorithm
 // A detailed explanation.
 
+// RECURSION
+
+// Recursion is a method of iterating over an operation by having a function call itself over and over again until it arrives at the intended result (base case) and stops.  If there is no stopping condition in a recursive function, it can become an infinite loop, which can crash your browser.
+
+// The benefit of recursive functions are when a function is needed repeatedly with different parameters from within a loop. 
+
+// An example of a recursive function is;
+
+function recurse() {
+  if(condition) {         // the base condition "base case" is met
+      recurse();         // recursively call back the function
+  }
+  else {                 // once the condition is met,
+      // stop calling recurse() - the function stops calling itself
+  }
+}
+
+recurse();
+
 //6. List two sorting algorithms,
 // explain in detail how both sorting algorithms work
 // explain the benefits and the cons of each sorting algorithm
 // Should take more than one paragraph to do this part
+
+// MERGE SORT
+
+// Merge sort works by splitting the list or input in half, and iterating through each half recursively, and then merging the sorted data back together.
+
+// Benefits: Super fast, as it breaks up the data into chunks and sorts them simultaneously, and runs on O(n lg(n)), which means that it scales as 'n' grows. 
+
+// Cons: Merge sort has a poor space complexity, taking up O(n) extra space because of the recursive call stack.
+
+
+// SELCTION SORT
+
+// Selection sort works by iterating through the data and repeatedly selecting the next smallest item from an unsorted array, and moving it to the front of the array.
+
+// Benefits: It is space efficient becasue it only requires a constant amount of additional space.
+
+// Cons: The time complexity for this sort is very poor, especially for very large data sets because it takes O(n2) time, even if the input is already sorted.
+
 
 /*  7.
  * flipBool takes an array of Boolean values
@@ -61,6 +102,8 @@ console.log(shop(wishlist));
  * Be sure to use map()!
  */
 const flipBool = (arr) => {};
+
+
 
 //Bonus:
 //FEAST OR FAMINE
