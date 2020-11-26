2003e2320bbb5cf50545efefff4cb4a2973f2aeb
Shan Siddiqui <shansiddiqui@Shans-Air.lan>
2020-11-25 19:13:53 -0500

    test

--- main.js
+++ main.js
@@ -12,15 +12,40 @@ const carBrands = [
   { brand: "Nissan", model: "frontier", type: "pickup" },
 ];
 
+const car = carBrands.filter(function (car) {
+  return car.type === "sedan";
+});
+console.log(car);
 /* 2.
  * reverseString takes a string
  * and should return the reverse of the string, you cannot use .reverse method
  * e.g., reverseString('cat') => 'tac'
  */
+function reverseString(str) {
+  let reverse = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverse += str[i];
+  }
+  return reverse;
+}
 
 // 3.
 // Write a function that takes an array of strings,
 // and returns the longest string in the array
+let arr = ["where", "we", "bout", "to", "eat", "at"];
+function hunger() {
+  let stuffed = "";
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= 5) {
+      stuffed.push(arr[i]);
+    }
+  }
+  return stuffed;
+}
+
+let hungry = arr.reduce((food, moreFood) => {
+  return food.length > moreFood.length;
+});
 
 // 4.
 // Using Reduce,
@@ -36,19 +61,25 @@ let wishlist = [
   { title: "tesla", price: 2000 },
   { title: "tesla", price: 90000 },
 ];
-function shop(arr) {}
-console.log(shop(wishlist));
+let shop = wishlist.reduce((acc, value) => {
+  return acc + value.price;
+}, 0);
 
+console.log(shop);
 // 5.
 //Explain recursion in your own words,
 // give an example of a recursive algorithm, and explain how it works
 //Make sure to detail the steps that make up a recursive algorithm
 // A detailed explanation.
+// ANSWER BELOW
+// A recursive function is a fuction that continues to call on itself until it doesnt. You set a base case which is the iteration for the recursion function, the function will continue to call itself until it meets the base case. We set the base case to prevent the function from going into an infinte loop.
 
 //6. List two sorting algorithms,
 // explain in detail how both sorting algorithms work
 // explain the benefits and the cons of each sorting algorithm
 // Should take more than one paragraph to do this part
+//Answer Below
+//  Here is how Bubble Sort works, If we were to look at an array of numbers arr = 9,7,5,2,6. Bubble Sort will look at the first two indexes 0, and 1, and if the number on the left is bigger than the right it will swap the number. This will continue to bubble up and keep going down the length of the array until all numbers are in squential order. Pros for Bubble Sort are that is can be used for external sorting, the cons for Bubble Sort are that it take a long time to run. Next we have MergeSort works like this it takes the "Divide and Conquer" approach. MergeSort will find the middle of the array and split it into two halves, it takes smaller chucks of the algo apart to solve the bigger picture. Pros for mergeSort is that it can quickly tackle large list, cons or worst case is it space complexity falls under o(n).
 
 /*  7.
  * flipBool takes an array of Boolean values
@@ -60,8 +91,15 @@ console.log(shop(wishlist));
  * then return the new array
  * Be sure to use map()!
  */
-const flipBool = (arr) => {};
-
+const flipBool = (arr) => {
+  arr.map(function (word) {
+    return !word;
+  });
+};
+flipBool = arr.map(function (word) {
+  return;
+  !word;
+});
 //Bonus:
 //FEAST OR FAMINE
 //  - Write a method that takes two arguments: an animal and a food, which are lowercase and have at least two letters each
@@ -70,3 +108,5 @@ const flipBool = (arr) => {};
 // > i.e.:
 // > input => `"great blue heron", "garlic naan"`
 // > output => `"gn"`
+
+function animalFood(animal, food) {}
