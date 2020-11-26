8288ae1bc493e15837f3e8182e1a1e5a444bd703
Ayman Omer <aymangebrilomer@gmail.com>
2020-11-25 19:12:57 -0500

    Update main.js

--- main.js
+++ main.js
@@ -162,9 +162,12 @@ let a =first.split('')
 let b= tow.split('')
 if(a[0]===b[0]&& a[a.length-1] ===b[b.length-1]){
  return `${a[0]+ a[a.length-1]} `
+}else{
+  return false;
 }
  
 
   
  }
  console.log(firstLast("great blue heron", "garlic naan"));
+ console.log((firstLast("breat blue heron", "garlic naan")))
